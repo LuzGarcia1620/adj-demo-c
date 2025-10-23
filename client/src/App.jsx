94142b9c6f3f56166039c4fde7b1c09286b1660b
LuzGarcia1620 <20223tn155@utez.edu.mx>
2025-10-22 19:51:54 -0600

    primer commit

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import TestController from './modules/test/test.controller'
+
+function App() {
+
+  return (
+    <>
+     <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid"><a href="" className="navbar-brand">ADJ-Demo</a>
+      <ul className="navbar-nav">
+        <li className="nav-item"><a href="" className="nav-link active" aria-current="page">Home</a></li>
+      </ul>
+      </div>
+     </nav>
+
+      <div className="container mt-5">
+        <h2>Inicio</h2>
+        <hr />
+        <button onClick={} } className="btn btn-succes">
+          llamar a mi APIsss
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/modules/test/test.controller.js
@@ -0,0 +1,18 @@
+
+
+const TestController = {}
+const ENV = import.meta.env
+const API_URL = `http://${ENV.VITE_API_HOST}:${ENV.VITE_API_PORT}${ENV.VITE_API_BASE}`;
+
+TestController.callToAPI = async () => {
+    await fetch(`${API_URL}/test` ,{
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }).then(res => res.json()).then(res=>{
+        console.log(res);
+    }).catch(err=> console.log);
+}
+export default TestController;
